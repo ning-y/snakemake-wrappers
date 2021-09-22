46c6b0fc799baa0adbb5fd1f85ad15f89d144950
Ning Yuan <ningyuan.sg@gmail.com>
2021-09-22 15:35:15 +0800

    Add epacts/make-gene-group-file

---
+++ epacts/make-gene-group-file/wrapper.R
@@ -0,0 +1,30 @@
+suppressPackageStartupMessages({
+  library(vcfR)
+  library(tidyverse)
+  library(assertthat)})
+
+assert_that(!is.null(snakemake@params[["info"]]))
+
+vcf <- read.vcfR(snakemake@input[[1]])
+tb <- as_tibble(getFIX(vcf)) %>%
+  mutate(genes=extract.info(vcf, snakemake@params[["info"]])) %>%
+  transmute(
+    key=str_glue("{CHROM}:{POS}_{REF}/{ALT}"),
+    # \x3b encodes ";", but vcfR reads it literally as R-style \\x3b
+    genes=str_split(genes, "\\\\x3b"))
+
+markers <- list()
+
+for (i in 1:nrow(tb)) {
+  key <- tb %>% pull(key) %>% nth(i)
+  genes <- tb %>% pull(genes) %>% nth(i) %>% unlist()
+  for (gene in genes) {
+    if (is.null(markers[[gene]])) markers[[gene]] <- key
+    else markers[[gene]] <- c(markers[[gene]], key)
+  }
+}
+
+lines <- names(markers) %>%
+  sapply(function(gene) paste0(c(gene, markers[[gene]]), collapse="\t"))
+
+write_lines(lines, snakemake@output[[1]])
