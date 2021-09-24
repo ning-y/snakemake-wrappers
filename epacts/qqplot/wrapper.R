71b09f5be81c85e5be5969a597eb8937ffaf9937
Ning Yuan <ningyuan.sg@gmail.com>
2021-09-24 18:59:15 +0800

    Fix epacts/qqplot

--- epacts/qqplot/wrapper.R
+++ epacts/qqplot/wrapper.R
@@ -3,11 +3,16 @@ suppressPackageStartupMessages({
   library(latex2exp)})
 
 results <- read_tsv(snakemake@input[[1]]) %>%
-  transmute(observed=-log10(PVALUE))
+  select(observed_p=PVALUE) %>%
+  arrange(observed_p) %>%
+  mutate(
+    expected_p=seq(from=0, to=1, length.out=n()),
+    observed_logp=-log10(observed_p),
+    expected_logp=-log10(expected_p))
 
-plot <- ggplot(results, aes(sample=observed)) +
-  stat_qq(distribution=qunif) +
-  stat_qq_line(distribution=qunif) +
-  xlab(TeX("Expected $\\log_{10}(p)$")) +
-  ylab(TeX("Observed $\\log_{10}(p)$"))
+plot <- ggplot(results, aes(x=expected_logp, y=observed_logp)) +
+  geom_point() +
+  geom_abline(aes(intercept=0, slope=1)) +
+  xlab(TeX("Expected $-\\log_{10}(p)$")) +
+  ylab(TeX("Observed $-\\log_{10}(p)$"))
 ggsave(snakemake@output[[1]], plot)
