4a005234587fc123f4522eaeb8a4bf6007560ada
Ning Yuan <ningyuan.sg@gmail.com>
2021-09-25 14:03:02 +0800

    Annotate plots with the genomic inflation factor

--- epacts/qqplot/wrapper.R
+++ epacts/qqplot/wrapper.R
@@ -10,9 +10,26 @@ results <- read_tsv(snakemake@input[[1]]) %>%
     observed_logp=-log10(observed_p),
     expected_logp=-log10(expected_p))
 
+# Calculate the genomic inflaction factor, \lambda_{GC}
+observed_median_chisq <- median(results$observed_p) %>% qchisq(., 1)
+expected_median_chisq <- qchisq(0.5, 1)
+genomic_inflation_factor <- observed_median_chisq / expected_median_chisq
+annotations <- tibble(
+  label=TeX(
+    sprintf("Genomic inflation factor $\\lambda_{\\mathrm{GC}} = %s$",
+            formatC(genomic_inflation_factor, digits=2, format="f")),
+    output="character"),
+  # Top-left corner
+  x=-Inf, y=Inf,
+  # Negative hjust direction moves text right, positive vjust is text down
+  hjust=-0.1, vjust=2)
+
 plot <- ggplot(results, aes(x=expected_logp, y=observed_logp)) +
   geom_point() +
   geom_abline(aes(intercept=0, slope=1)) +
+  geom_text(
+    data=annotations, parse=TRUE,
+    mapping=aes(x=x, y=y, hjust=hjust, vjust=vjust, label=label)) +
   xlab(TeX("Expected $-\\log_{10}(p)$")) +
   ylab(TeX("Observed $-\\log_{10}(p)$"))
 ggsave(snakemake@output[[1]], plot)
