ce06eeda07477b6f76dccf9f9b915c038938a2a2
Ning Yuan <ningyuan.sg@gmail.com>
2021-12-27 10:32:26 +0800

    Add wrapper bcftools/rename-chroms

---
+++ bcftools/rename-chroms/wrapper.py
@@ -0,0 +1,26 @@
+__author__ = "Ning Yuan"
+
+import re
+from snakemake.shell import shell
+
+assert len(snakemake.output) == 2
+assert hasattr(snakemake.output, "vcf")
+assert hasattr(snakemake.output, "tmp")
+assert len(snakemake.input) == 1
+assert hasattr(snakemake.params, "rename")
+
+output_type = "z" if re.search("vcf\.gz$", snakemake.output[0]) else \
+    "v" if re.search("\.vcf$", snakemake.output[0]) else None
+if output_type is None: raise ValueError("Bad output extension")
+
+with open(snakemake.output.tmp, "w") as txtfile:
+    lines_to_write = [f"{old}\t{new}" for old, new in snakemake.params.rename.items()]
+    text_to_write = "\n".join(lines_to_write)
+    txtfile.write(text_to_write)
+
+shell(
+    f"""
+    bcftools annotate --output-type {output_type} \
+        --rename-chrs {snakemake.output.tmp} \
+        {snakemake.input} > {snakemake.output.vcf}
+    """)
