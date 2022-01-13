ee3f551651f4dad9e602ba0faf4a9b5309de5ded
Ning Yuan <ningyuan.sg@gmail.com>
2022-01-13 17:10:18 +0800

    Provide option to keep variants with zero AC

--- bcftools/subset-samples/wrapper.py
+++ bcftools/subset-samples/wrapper.py
@@ -11,10 +11,15 @@ assert len(snakemake.output) == 1
 assert len(snakemake.input) == 1
 if output_type is None: raise ValueError("Bad output extension")
 
+if not hasattr(snakemake.params, "remove_zero_allele_counts") or snakemake.params.remove_zero_allele_counts:
+    maybe_remove_ac_zero = "--exclude \"INFO/AC=0\""
+else:
+    maybe_remove_ac_zero = ""
+
 shell(
     f"""
     bcftools view --samples {samples} {snakemake.input} | \
         bcftools annotate --remove INFO/AC,INFO/AN | \
         bcftools +fill-tags -- --tags AC,AN | \
-        bcftools filter --output-type {output_type} --exclude "INFO/AC=0" > {snakemake.output}
+        bcftools filter --output-type {output_type} {maybe_remove_ac_zero} > {snakemake.output}
     """)
